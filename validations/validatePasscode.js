55e1d58653b9711dbc63c6e447a4dccc65b41d58
nwofeemmanuel1 <enwofe2020@gmail.com>
2022-03-14 13:56:02 +0100

    changes

--- validations/validatePasscode.js
+++ validations/validatePasscode.js
@@ -2,7 +2,7 @@ const Joi=require("joi")
 
 const validatePcode=(req)=>{
     const schema=Joi.object({
-        passcode:Joi.string().required().max(10).min(3)
+        passcode:Joi.string().required().max(10).min(6)
     })
 
     const result=schema.validate({passcode:req.passcode})
